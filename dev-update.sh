82f78834c4ead264cb188f240f639c7ab4e1f2ba
Cooper Dukes <cooper@78.23.129.180.unknown.m1.com.sg>
2014-02-23 12:50:17 +0800

    added maintenance script

---
+++ dev-update.sh
@@ -0,0 +1,24 @@
+# Update all WP cores
+find ~/Sites -type d -maxdepth 1 -exec wp core update --path={} \;
+find ~/Sites -type d -maxdepth 1 -exec wp core update-db --path={} \;
+
+# Update all plugins
+find ~/Sites -type d -maxdepth 1 -exec wp plugin update-all --path={} \;
+
+# Update all themes
+find ~/Sites -type d -maxdepth 1 -exec wp theme update-all --path={} \;
+
+# Update all submodules
+find ~/Documents -type d -name '.git' -exec sh -c 'cd "{}" && cd .. && git submodule foreach git pull origin master' \;
+
+# Update all Bower dependencies
+find ~/Documents -type d -name 'bower_components' -exec sh -c 'cd "{}" && cd .. && bower update --save' \;
+
+# Flush all WP caches
+find ~/Sites -type d -maxdepth 1 -exec wp cache flush --path={} \;
+
+# Flush all WP rewrites
+find ~/Sites -type d -maxdepth 1 -exec wp rewrite flush --path={} \;
+
+# Regenerate all thumbnails
+find ~/Sites -type d -maxdepth 1 -exec wp media regenerate --path={} \;
